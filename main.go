dd117b4ffa36d72989b007c3a166c158084b58fa
NazarovAA <nazarovandry2@mail.ru>
2019-05-23 10:54:13 +0300

    try

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+/*package main
 
 import (
 	//"log"
@@ -58,7 +58,7 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-/*func main() {
+func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -76,3 +76,88 @@ func main() {
 
 	router.Run(":" + port)
 }*/
+
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	r := gin.Default()
+	store := sessions.NewCookieStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+
+	r.POST("/login", login)
+	r.GET("/logout", logout)
+	private := r.Group("/private")
+	{
+		private.GET("/", private1)
+		private.GET("/two", private2)
+	}
+	private.Use(AuthRequired())
+	r.Run(":8080")
+}
+
+func AuthRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		user := session.Get("user")
+		if user == nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid session token"})
+		} else {
+			c.Next()
+		}
+	}
+}
+
+func login(c *gin.Context) {
+	session := sessions.Default(c)
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
+	if strings.Trim(username, " ") == "" || strings.Trim(password, " ") == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Parameters can't be empty"})
+		return
+	}
+	if username == "hello" && password == "itsme" {
+		session.Set("user", username)
+		err := session.Save()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate session token"})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"message": "Successfully authenticated user"})
+		}
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
+	}
+}
+
+func logout(c *gin.Context) {
+	session := sessions.Default(c)
+	user := session.Get("user")
+	if user == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid session token"})
+	} else {
+		log.Println(user)
+		session.Delete("user")
+		session.Save()
+		c.JSON(http.StatusOK, gin.H{"message": "Successfully logged out"})
+	}
+}
+
+func private1(c *gin.Context) {
+	session := sessions.Default(c)
+	user := session.Get("user")
+	c.JSON(http.StatusOK, gin.H{"hello": user})
+}
+
+func private2(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"hello": "Logged in user"})
+}
